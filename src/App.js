8a4aa8d1375c494de4dd3774a8b5ebcf1776aebe
SamiiKeller <samiikeller@hotmail.com>
2018-11-01 17:42:57 -0600

    First commit
    Holi Uriel

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var image = <img src={logo} className="App-logo" alt="logo" />;
+
 class App extends Component {
+  myName = "Uriel";
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {image}
+          {this.renderEditContent("src/file.js")}
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            {this.myName}
           </a>
         </header>
       </div>
+    );
+  }
+
+  renderEditContent(file) {
+    return (
+      <p>
+        Edit <code>{file}</code> and save to reload.
+      </p>
     );
   }
 }
